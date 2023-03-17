9d89b08fa507464c6cc02cd54627853343d7195f
unknown <arminfrontend@gmail.com>
2023-03-17 22:37:09 +0330

    Feat: Implemented inputs container

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,6 +5,9 @@ const Form = () => {
     <div className="main-container">
         {/* form container  */}
         <div className="bg-violet-400 mt-7 rounded-md pr-4 pl-2 py-2">
+            {/* inputs container  */}
+            <div className="flex flex-wrap gap-5 max-w-3xl">
+            </div>
         </div>
     </div>
     )
