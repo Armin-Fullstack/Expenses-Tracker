7aa464a4c37db618abaa72eb852fe38694db7f48
unknown <arminfrontend@gmail.com>
2023-03-17 22:22:06 +0330

    Feat: Implemented form container

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,6 +3,9 @@ const Form = () => {
     return (
     //   Main container 
     <div className="main-container">
+        {/* form container  */}
+        <div className="bg-violet-400 mt-7 rounded-md pr-4 pl-2 py-2">
+        </div>
     </div>
     )
   }
