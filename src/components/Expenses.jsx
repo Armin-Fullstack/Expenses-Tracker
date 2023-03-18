57f731c5936c715c6c956735c25378f05c80327c
unknown <arminfrontend@gmail.com>
2023-03-18 18:08:51 +0330

    Feat: Implemented expenses container

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -4,6 +4,9 @@ const Expenses = () => {
   return (
     // main container 
     <div className="main-container mt-8">
+        {/* expenses container  */}
+        <div className="bg-[rgba(0,0,0,0.85)] flex flex-col rounded-md px-3 py-6 space-y-4">
+        </div>
     </div>
   )
 }
