f25e3d2e76b7cc973ed9219af6876e0b0d4bc158
unknown <arminfrontend@gmail.com>
2023-03-18 19:30:37 +0330

    Feat: Completed ExpensesItem UI

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -5,7 +5,7 @@ const Expenses = () => {
     // main container 
     <div className="main-container mt-8">
         {/* expenses container  */}
-        <div className="bg-[rgba(0,0,0,0.85)] flex flex-col rounded-md px-3 py-6 space-y-4">
+        <div className="bg-[rgba(0,0,0,0.6)] flex flex-col rounded-md px-3 py-6 space-y-4">
             {/* filter section  */}
 
             {/* chart section  */}
--- src/components/ExpensesItem.jsx
+++ src/components/ExpensesItem.jsx
@@ -2,16 +2,21 @@
 const ExpensesItem = () => {
     return (
     //   expense item container 
-    <div className="bg-zinc-600 flex justify-between rounded-md p-2">
+    <div className="bg-zinc-600 flex justify-between items-center rounded-md p-2">
         {/* date and title container  */}
-        <div className="flex space-x-2">
+        <div className="flex space-x-3 items-center">
             {/* date  */}
             <div className="bg-[rgba(0,0,0,0.85)] border-2 border-white text-white py-4 px-6 rounded-lg text-center">
                 <p className="font-bold text-sm">August</p>
                 <p className="text-xs mt-1">2023</p>
                 <p className="font-bold mt-2 text-2xl md:text-3xl">14</p>
             </div>
+            {/* title  */}
+            <h2 className="text-white text-xl font-bold">Toilet Paper</h2>
         </div>
+
+        {/* price  */}
+        <span className="border rounded-lg px-5 py-1 md:px-7 md:py-2 bg-purple-900 text-white">$94.12</span>
     </div>
     )
   }
