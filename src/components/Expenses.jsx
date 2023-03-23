50dc5a4989532bb43e31f582039ed6d90765ab8c
unknown <arminfrontend@gmail.com>
2023-03-23 17:14:48 +0430

    Feat: Added filterYear state

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import ExpensesItem from "./ExpensesItem"
 
 const Expenses = () => {
+  const [filterYear , setFilterYear] = useState("2019")
   return (
     // main container 
     <div className="main-container mt-8">
@@ -9,7 +11,7 @@ const Expenses = () => {
             {/* filter section  */}
             <div className="flex justify-between items-center mb-5">
               <p className="text-white font-bold">Filter by year</p>
-              <select className="px-8 py-2 rounded-sm focus:outline-none">
+              <select onChange={(e) => setFilterYear(e.target.value)} className="px-8 py-2 rounded-sm focus:outline-none" >
                 <option value="2019">2019</option>
                 <option value="2020">2020</option>
                 <option value="2021">2021</option>
