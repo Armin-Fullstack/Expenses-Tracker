cad36d70f1437d9542b2ef6175ee654df5b98686
unknown <arminfrontend@gmail.com>
2023-03-23 16:54:40 +0430

    Feat: Created filter section

--- src/components/Expenses.jsx
+++ src/components/Expenses.jsx
@@ -7,7 +7,15 @@ const Expenses = () => {
         {/* expenses container  */}
         <div className="bg-[rgba(0,0,0,0.6)] flex flex-col rounded-md px-3 py-6 space-y-4">
             {/* filter section  */}
-
+            <div className="flex justify-between items-center mb-5">
+              <p className="text-white font-bold">Filter by year</p>
+              <select className="px-8 py-2 rounded-sm focus:outline-none">
+                <option value="2019">2019</option>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2022">2022</option>
+              </select>
+            </div>
             {/* chart section  */}
 
             <ExpensesItem/>
